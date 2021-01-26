758818f4fb33f21929ec87d147324e97146c8841
Saif Gorges <69221324+saif-gorges@users.noreply.github.com>
2021-01-26 16:33:34 -0500

    Added starter code template

--- Solved/app.js
+++ Solved/app.js
@@ -1,4 +1,3 @@
-// @TODO: YOUR CODE HERE!
 var svgWidth = 960;
 var svgHeight = 500;
 
@@ -10,12 +9,12 @@ var margin = {
 };
 
 var width = svgWidth - margin.left - margin.right;
-var height = svgHeight - margin.top - margin.bottom;djn
+var height = svgHeight - margin.top - margin.bottom;
 
 // Create an SVG wrapper, append an SVG group that will hold our chart,
 // and shift the latter by left and top margins.
 var svg = d3
-  .select("#scatter")
+  .select(".chart")
   .append("svg")
   .attr("width", svgWidth)
   .attr("height", svgHeight);
@@ -25,14 +24,14 @@ var chartGroup = svg.append("g")
   .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
 // Initial Params
-var chosenXAxis = "poverty";
+var chosenXAxis = "hair_length";
 
 // function used for updating x-scale var upon click on axis label
-function xScale(healthcareLow, chosenXAxis) {
+function xScale(hairData, chosenXAxis) {
   // create scales
   var xLinearScale = d3.scaleLinear()
-    .domain([d3.min(healthcareLow, d => d[chosenXAxis]) * 0.8,
-      d3.max(healthcareLow, d => d[chosenXAxis]) * 1.2
+    .domain([d3.min(hairData, d => d[chosenXAxis]) * 0.8,
+      d3.max(hairData, d => d[chosenXAxis]) * 1.2
     ])
     .range([0, width]);
 
